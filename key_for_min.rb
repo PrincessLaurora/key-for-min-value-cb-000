f0b43e81b38ada9f60c2e9059f79c333581800b7
Laura Felce <crepu83@yahoo.it>
2018-11-23 10:56:22 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,8 +4,7 @@
 def key_for_min_value(name_hash)
 smallest_value = ""
 name_hash.each do |key, value|
-  if smallest_value < value
-    return key
+  if key["smallest_value"] < key["value"]
+    return key["smallest_value"]
 end
 end
-
